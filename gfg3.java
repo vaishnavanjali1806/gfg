fa1fb8828ea6ec493f2208117608fc2c530de4a8
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-23 12:26:13 +0530

    Update gfg3.java

--- gfg3.java
+++ gfg3.java
@@ -1,1 +1,28 @@
 
+   int n = arr.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int[] result = new int[n];
+        
+       
+        for (int i = n - 1; i >= 0; i--) {
+          
+            while (!st.isEmpty() && st.peek() <= arr[i]) {
+                st.pop();
+            }
+            
+          
+            result[i] = st.isEmpty() ? -1 : st.peek();
+            
+            
+            st.push(arr[i]);
+        }
+        
+      
+        for (int num : result) {
+            list.add(num);
+        }
+        
+        return list;
+}
+}
