6ccf8a786799f60fda3ff699eed3b018d190e270
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-24 19:55:48 +0530

    Update gfg4.java

--- gfg4.java
+++ gfg4.java
@@ -1,1 +1,15 @@
-
+  ArrayList<Integer> list = new  ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int l=arr.length;
+        for(int i=0;i<l;i++){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+            if(!st.isEmpty())list.add(i-st.peek());
+            else list.add(i+1);
+            
+            st.push(i);        
+        }
+        return list;
+    }
+}
