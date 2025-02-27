751a715c3f47cc27f9704f0c91f5f937eba6a6a1
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-27 22:57:43 +0530

    Update gfg7.java

--- gfg7.java
+++ gfg7.java
@@ -1,1 +1,45 @@
+  class Solution {
+    Stack<Integer> st = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
 
+
+    public Solution() {}
+
+    // Add an element to the top of Stack
+    public void push(int x) {
+        st.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+        
+    }
+
+    // Remove the top element from the Stack
+    public void pop() {
+        // code here
+         if (!st.isEmpty()) {
+            int removed = st.pop();
+            if (removed == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+
+    // Returns top element of the Stack
+    public int peek() {
+        // code here
+        if (st.isEmpty()) {
+            return -1;
+        }
+        return st.peek();
+    }
+
+    // Finds minimum element of Stack
+    public int getMin() {
+        // code here
+          if (minStack.isEmpty()) {
+            return -1;
+        }
+        return minStack.peek();
+    }
+}
