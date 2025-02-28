f9d42588cdfd465d3ac8710055db4f3c692a4c18
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-28 22:23:26 +0530

    Update gfg8.java

--- gfg8.java
+++ gfg8.java
@@ -1,1 +1,33 @@
+ Stack<Integer> st = new Stack<>();
+        for(String s : arr){
+            if(Character.isDigit(s.charAt(0)) || 
+            (s.length() > 1 && s.charAt(0) == '-' && 
+            Character.isDigit(s.charAt(1)))){
+                st.push(Integer.parseInt(s));
+            }else{
+                int b = st.pop();
+                int a = st.pop();
+                int c = 0;
+                
+                switch(s){
+                    case "+":
+                        c = a + b;
+                        break;
+                    case "-":
+                        c = a - b;
+                        break;
+                    case "*":
+                        c = a * b;
+                        break;
+                    case "/":
+                        c = a / b;
+                        break;
+                }
+                
+                st.push(c);
+            }
+        }
+        return st.pop();
 
+}
+}
