6246c3410e00d7f8a64960002142d903ddbce9af
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-03-02 22:55:34 +0530

    Update gfg10.java

--- gfg10.java
+++ gfg10.java
@@ -1,1 +1,16 @@
-
+  Deque<Integer> dq = new ArrayDeque<>();
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i =0; i < n; i++) {
+            while(dq.size() != 0 && arr[dq.getLast()] < arr[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            if(i - dq.getFirst() == k) dq.removeFirst();
+            if(i >= k -1) {
+                ans.add(arr[dq.getFirst()]);
+            }
+        }
+        return ans;
+}
+}
