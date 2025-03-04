6615b7138f4ebcadfb370207529798614802eac9
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-03-04 19:10:26 +0530

    Update gfg12.java

--- gfg12.java
+++ gfg12.java
@@ -1,1 +1,21 @@
+  int n = arr.length;
+        if (n == 0) {
+            return 0; 
+        }
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1); 
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxLen = 0;
+        for (int len : dp) {
+            maxLen = Math.max(maxLen, len);
+        }
+        return maxLen;
 
