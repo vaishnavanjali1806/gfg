e7e315762cadb89324bffdc5f7227932f4aba5a0
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-03-09 23:08:52 +0530

    Update gfg17.java

--- gfg17.java
+++ gfg17.java
@@ -1,1 +1,25 @@
-
+ int n = s.length();
+        int count = 0;
+        int l,r = 0;
+        for(int i = 0;i<n;i++)
+        {
+            //odd Length Palindrome
+            l = i-1;
+            r = i;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r))
+            {
+                count++;
+                l--;
+                r++;
+            }
+            //Even Length Palindrome
+            l = i-1;
+            r = i+1;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r))
+            {
+                count++;
+                l--;
+                r++;
+            }
+        }
+        return count;
