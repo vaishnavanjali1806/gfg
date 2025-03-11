a3aef80688727136d3938f4dffe1cff7be1f401e
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-03-11 20:35:35 +0530

    Update gfg19.java

--- gfg19.java
+++ gfg19.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    int countWays(int n) {
+        // your code here
+         if(n==1)
+        {
+            return 1;
+        }
+        if(n==2)
+        {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int result = 0;
+        for(int i=3;i<=n;i++)
+        {
+            result = a+b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
+}
