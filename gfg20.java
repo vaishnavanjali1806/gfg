fc8e50b3ce91a65b07641085f80bbd9044946170
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-03-12 21:11:38 +0530

    Update gfg20.java

--- gfg20.java
+++ gfg20.java
@@ -1,1 +1,8 @@
-
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        // Write your code here
+          int n=cost.length;
+        for(int i=2;i<n;i++)cost[i]+=Math.min(cost[i-1],cost[i-2]);
+        return Math.min(cost[n-1],cost[n-2]);
+    }
+};
