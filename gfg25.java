f28354a6c720d5bbd41e8e4a4cfc8793440d019f
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-03-21 21:39:00 +0530

    Update gfg25.java

--- gfg25.java
+++ gfg25.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int findMaxSum(int nums[]) {
+        // code here
+           int prev2=0;
+        int prev=nums[0];
+        for(int i=1;i<nums.length;i++){
+            int take=nums[i];
+            if(i>1)take+=prev2;
+            int notTake=0+prev;
+            int curr=Math.max(take,notTake);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+    }
+}
