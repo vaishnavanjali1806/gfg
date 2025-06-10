d863d31c486703201552ab7809511fb73df29158
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-06-10 20:12:43 +0530

    Update gfg31.java

--- gfg31.java
+++ gfg31.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    int countStrings(String s) {
+        // code here
+         int[] a=new int[26];
+        for(char c: s.toCharArray())
+        a[c-'a']++;
+        int ans=0,extra=0;
+        int l=s.length();
+        for(int c : a){
+            if(c>1)extra=1;
+            ans+=(l-c)*c;
+            l-=c;
+        }
+        return ans+extra;
+    }
+}
