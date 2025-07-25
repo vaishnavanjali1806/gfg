0449806c9aa61db1ad7f5fefd4b0e94b55aa739e
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-07-25 15:53:31 +0530

    Update gfg36.java

--- gfg36.java
+++ gfg36.java
@@ -1,1 +1,28 @@
+  int n = arr.length;
+        
+       
+        int maxKadane = kadane(arr);
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            totalSum += arr[i];
+            arr[i] = -arr[i]; 
+        }
 
+        int maxInvertedKadane = kadane(arr); 
+        int maxWrap = totalSum + maxInvertedKadane; 
+      
+        if (maxWrap == 0) {
+            return maxKadane;
+        }
+        
+        return Math.max(maxKadane, maxWrap);
+    }
+    private int kadane(int[] arr) {
+        int maxEndingHere = arr[0];
+        int maxSoFar = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+        
