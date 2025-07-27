b54a1a7c5694cf7457faaeb078f26e86f7032eba
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-07-27 12:25:55 +0530

    Update gfg37.java

--- gfg37.java
+++ gfg37.java
@@ -1,1 +1,13 @@
+class Solution {
+    public Pair<Integer, Integer> getMinMax(int[] arr) {
+    
+        // Code Here
+        Arrays.sort(arr);
+    int n = arr.length;
+    int min = arr[0];
+    int max = arr[n-1];
+    return new Pair<>(min,max);
+     
+    }
+}
 
