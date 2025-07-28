9edd8bfbaf9ad103a13e3ebfa947362d5d7dd441
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-07-28 17:35:54 +0530

    Update gfg39.java

--- gfg39.java
+++ gfg39.java
@@ -1,1 +1,22 @@
 
+
+class Solution {
+    public int findMinDiff(ArrayList<Integer> arr, int m) {
+       
+         int ans=Integer.MAX_VALUE;
+          Collections.sort(arr);
+          int n=arr.size();
+          
+          for(int i=0 ; i<n-m+1 ;i++){
+               
+               int minelement=arr.get(i);
+               int maxelement=arr.get(i+m-1);
+               ans=Math.min(ans,maxelement-minelement);
+               
+          }
+          
+          return ans;
+    }
+}
+
+ 
