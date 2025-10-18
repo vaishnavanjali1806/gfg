9e664cf07e9ec859e9fec8d0616600a04fd1f50c
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-10-18 21:36:49 +0530

    Add inorder traversal and findMedian method
    Implement inorder traversal and median finding.

---
+++ gfg130.java
@@ -0,0 +1,20 @@
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public void inorder(Node root){
+        if(root == null) return;
+        inorder(root.left);
+        list.add(root.data);
+        inorder(root.right);
+    }
+    public int findMedian(Node root) {
+        // Code here
+        inorder(root);
+        int n = list.size();
+        if(n % 2 == 0){
+            return list.get((n/2)-1);
+        }
+        else{
+            return list.get(((n+1)/2)-1);
+        }
+    }
+}
