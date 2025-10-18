fc13c3a0c228004ccaf27a3fb8866e8a0c89ece1
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-10-18 21:37:15 +0530

    Remove unused Node class from gfg105.java
    Removed commented-out Node class definition.

--- gfg105.java
+++ gfg105.java
@@ -1,13 +1,7 @@
 /*
 class Node{
     int data;
-    Node left, right;
-    Node(int d){
-        data=d;
-        left=right=null;
-    }
-}
-*/
+
 
 class Solution {
         long max, min = Integer.MIN_VALUE;
