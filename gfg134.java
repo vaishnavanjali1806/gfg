c6e79cd1a7f5bc26cc8e6eb3c4da3a08248a7733
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-10-22 23:19:27 +0530

    Create gfg134.java

---
+++ gfg134.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void nearlySorted(int[] arr, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // min heap
+        
+        for(int i=0; i<=k; i++){
+            // insert k elements from starting into pq
+            pq.offer(arr[i]);
+        }
+        
+        int index = 0; // for filling up the arr in sorted order
+        for(int i=k+1; i<arr.length; i++){
+            arr[index++] = pq.poll(); // extract min and store in arr
+            pq.add(arr[i]); // also store the next element in pq for further processing
+        }
+        
+        while(!pq.isEmpty()){
+            // store the remaining elements to arr
+            arr[index++] = pq.poll();
+        }
+    }
+}
